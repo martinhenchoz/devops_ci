428810f5aade35184b4632d003fb06a7cbd7d23b
Martin Henchoz <mhenchoz@yahoo.com>
2023-08-15 15:23:46 -0300

    Mensaje modificado

--- billing/src/main/java/com/paymentchain/billing/controller/InvoiceRestController.java
+++ billing/src/main/java/com/paymentchain/billing/controller/InvoiceRestController.java
@@ -48,7 +48,7 @@ public class InvoiceRestController {
     
     @ApiOperation(value = "Return all transaction bundled into Response", notes = "Return 204 if no data found")
     @ApiResponses(value = {
-        @ApiResponse(code = 204, message = "There are not transactions"),
+        @ApiResponse(code = 204, message = "No hay transacciones"),
         @ApiResponse(code = 500, message = "Internal error")})
     @GetMapping()
     public List<InvoiceResponse> list() {
